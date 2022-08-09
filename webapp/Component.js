aa3339617e6081090f29011b8f791f6f6701cfcf
intelira <prakash.saurav2812@gmail.com>
2022-08-09 00:44:06 +0000

    initial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("tech.intelira.phonebook.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
